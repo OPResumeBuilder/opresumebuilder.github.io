e7a5c8741a5ae2aa16021fc2dc9a9787764b77ec
pritam dutta <rohitdutta907@gmail.com>
2022-07-30 12:33:48 -0700

    Improving some of the functionality like remove button apearing and dissapearing to make it easy to cope with the framework did some initial design changes in desing add element button

--- Readme.md
+++ Readme.md
@@ -2,9 +2,25 @@ To add you resume design simply make a seperate folder and add them will include
 
 Structure :
 ```
-<button class='add_element' style="position: absolute;" type="button" onclick="add_element(this, 
-                    `
-                    HTML code you want to add
-                    `
-                )">Add Skills</button>
+<div class="info_collector">
+    <button class='add_element' type="button" onclick="add_element(this, 
+        `
+        HTML code
+        `
+        )">Add Contact Details</button>
+</div>
 ```
+info_collector - is must have in the parrent class of the "add_element"
+
+
+## How to add Remove button
+    `Here is an example`
+    ``<input type='text' on onfocusout='setTimeout(del_remove_button, 2000);' onclick=' add_remove_button(this); ' value='Insert Skill'>``
+
+    call del_remove_button(this) in onfocusout so that we can remove the button when we are moving out of focus
+
+# Requirement 
+
+    1. add op_resume_functions.js at the top of the html file in header which is holding all the functions
+    2. add op_resume.js at the buttom of the html file in header which is holding just the needed codes
+    3. Need to add op_style.css in the header
---
+++ op_script_functions.js
@@ -0,0 +1,60 @@
+// auto resize element 
+function resizable(el) {
+
+    function resize() {
+        el.style.width = el.value.length + 'ch'
+    }
+    var e = 'keyup,keypress,focus,blur,change'.split(',');
+    for (var i in e) el.addEventListener(e[i], resize, false);
+    resize();
+}
+
+function resize_all_inpiut() {
+    var all_inputs = document.body.getElementsByTagName('input');
+    Array.from(all_inputs).forEach(input => {
+        resizable(input);
+    });
+}
+
+// functions for removing and and adding remove element button
+function add_remove_button(element) {
+    parrent = element.parentNode;
+    // console.log(parrent);
+    let remove_buton = parrent.getElementsByClassName('remove_button');
+    console.log('found elemetn' + remove_buton[0])
+    if (remove_buton[0] == null) {
+        element.insertAdjacentHTML('afterend', '<button class="remove_button" onClick="remove_me(this)">remove</button>');
+    }
+
+}
+
+// END
+
+// add element function
+function add_element(element, element_to_add) {
+    Parrent = element.parentNode;
+
+    // element_to_add += '<button class="remove_button" onClick="remove_me(this)">remove</button>';
+    console.log(element_to_add);
+
+    var div = '<div style="width:100%;" class="float_remove_button">';
+    div += element_to_add;
+    div += '</div>'
+
+    Parrent.insertAdjacentHTML('afterbegin', div);
+    resize_all_inpiut();
+};
+
+// remove parrent element 
+function remove_me(element) {
+    parent = element.parentNode;
+    // console.log(element.previousSibling);
+    parent.parentNode.removeChild(parent);
+}
+
+function Focus($id) {
+    var f_element = document.getElementById($id);
+    console.log(f_element);
+    // window.scroll(0, f_element.offsetHeight);
+    f_element.scrollIntoView()
+}
